d9e0c35b241be237b567ecd9ee2c402320e2b3b0
maarish441-crypto <maarish441@gmail.com>
2023-04-12 00:27:34 +0530

    first commit

---
+++ src/OOPS.java
@@ -0,0 +1,15 @@
+class shape{
+    String color;
+}
+
+class triangle extends shape{   // inheritence
+
+}
+
+public class OOPS{
+    public static void main(String args[]){
+        triangle t1 = new triangle();
+        t1.color = "red";
+    }
+}
+
